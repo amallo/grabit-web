7d246ad3b7fcbd97214b2deff7273b9a62913ef2
AM <audie@app2b.io>
2024-04-03 13:35:39 +0200

    some tries on UI/UX

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,69 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { FormControl, FormLabel, FormHelperText, Textarea, Button, Text, Input } from '@chakra-ui/react';
+import { CopyIcon } from '@chakra-ui/icons';
 
 function App() {
+  const [message, setMessage] = useState<string | null>(null)
+  const [willDropMessage, setDroppedMessage] = useState<string | null>(null)
+  const refInputMessage = useRef<HTMLTextAreaElement>(null)
+  const dropMessage = ()=>{
+    setDroppedMessage(message)
+    setMessage(null)  
+  }
+  const cancelMessage = ()=>{
+    setMessage(null)  
+    if (refInputMessage.current){
+      refInputMessage.current.value = ""
+    }
+  }
+  const restart = ()=>{
+    setMessage(null)  
+    if (refInputMessage.current){
+      refInputMessage.current.value = ""
+    }
+    setDroppedMessage(null)
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <img src={logo} alt="logo" />
+          <div style={{display: 'flex', flex: 1, gap: 16}}>
+            <FormControl gap={16}>
+              <FormLabel>Déposer un message privé</FormLabel>
+              {!willDropMessage &&<>
+                <Textarea ref={refInputMessage} placeholder='Entrez ici votre message' onChange={((e)=>setMessage(e.currentTarget.value))} />
+                <FormHelperText>Votre message sera immédiatement détruit dès sa première lecture.</FormHelperText>
+              </>
+              }
+             
+              
+              
+
+              {willDropMessage && 
+              <Input type='text' readOnly value="http://grabit.com/2H43Gdd775Gkml66v" backgroundColor={'gray'} />              
+              }
+              
+              {willDropMessage && 
+              <FormHelperText color="GrayText" margin={0}>Vous pouvez envoyer ce lien dès maintenant à la personne de votre choix</FormHelperText>            
+              }
+              <div style={{display: 'flex', flex: 1, justifyContent: 'space-between', marginTop: 16 }}>
+              { !willDropMessage &&
+                <Button colorScheme='teal' onClick={()=> dropMessage()}>Déposer le message</Button>
+              }
+                { message &&
+                  <Button colorScheme='gray' onClick={()=>cancelMessage()}>Annuler</Button>
+                }
+                { willDropMessage &&
+                  <Button leftIcon={<CopyIcon/>}  onClick={()=>restart()}>Copier le lien</Button>
+                }
+                { willDropMessage &&
+                  <Button colorScheme='gray' onClick={()=>restart()}>Déposer un autre message</Button>
+                }
+              </div>
+            </FormControl>
+          </div>
       </header>
     </div>
   );
